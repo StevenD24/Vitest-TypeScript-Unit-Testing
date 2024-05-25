90c60c5fdac1977030d498807b08cb58ee1c95ff
Steven Duong <stevenduongx@gmail.com>
2024-05-25 16:05:54 -0600

    add unit tests for calculateAvg function

--- src/math.ts
+++ src/math.ts
@@ -6,3 +6,12 @@ export function multiply(n1: number, n2: number) : number {
     return n1 * n2;
 }
 
+export function calculateAverage(grades: number[]): number {
+    let sum = 0;
+    for (let i = 0; i < grades.length; i++) {
+        sum += grades[i];
+    }
+
+    return sum/grades.length;
+}
+
--- test/math.test.ts
+++ test/math.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from 'vitest';
-import { add, multiply } from '../src/math';
+import { add, calculateAverage, multiply } from '../src/math';
 
 test('add(1, 2) -> 3', () => {
     expect(add(1, 2)).toBe(3);
@@ -7,5 +7,25 @@ test('add(1, 2) -> 3', () => {
 
 test('add(2, 3) -> 6', () => {
     expect(multiply(2, 3)).toBe(6);
+});
+
+test('[] -> NaN', () => {
+    expect(calculateAverage([])).toBe(NaN);
+});
+
+test('[1]', () => {
+    expect(calculateAverage([1])).toBe(1);
+});
+
+test('[1, 2, 3]', () => {
+    expect(calculateAverage([1])).toBe(1);
+});
+
+test('[-1, 4, 3]', () => {
+    expect(calculateAverage([-1, 4, 3])).toBe(2);
+});
+
+test('[-1, 4, -3]', () => {
+    expect(calculateAverage([-1, 4, -3])).toBe(0);
 });
 
